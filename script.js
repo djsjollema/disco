c0a680fcb4617d5cff55ef20a1b5ef1ee9949d76
Jelle Sjollema <j.sjollema@ma-web.nl>
2016-01-14 14:11:07 +0100

    totaal

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-window.addEventListener("load",function(){ 
-	var myName = document.getElementById("myName");
-    var letters = ["j","e","l","l","e"];
+window.addEventListener("load", function () {
+    var myName = document.getElementById("myName");
+    var letters = ["J", "e", "l", "l", "e"," ",
+                   "S", "j", "o","l","l","e","m","a"];
     //myName.innerHTML = "blah blah";
     //myName.style.color = "red";
-    
-    for(teller= 0; teller<5; teller++){
-        var letter = document.createElement('span');
-        letter.innerHTML = letters[teller];
-        letter.style.color = "rgb(" + rndGetal() + ",0,0)"
-        myName.appendChild(letter);
+    var muziek = new Audio();
+     muziek.src = "media/Tommy Roe - Dizzy.mp3";
+    muziek.play();
+
+    function animate() {
+        myName.innerHTML = "";
+        for (teller = 0; teller < letters.length; teller++) {
+            var letter = document.createElement('span');
+            letter.innerHTML = letters[teller];
+            letter.style.color = "rgb(" + rndGetal() + "," + rndGetal() + "," + rndGetal() + ")";
+            myName.appendChild(letter);
+        }
     }
+    setInterval(animate, 100);
+
 })
 
-function rndGetal(){
-    return Math.floor(Math.random()*255);
+function rndGetal() {
+    return Math.floor(Math.random() * 255);
 }
